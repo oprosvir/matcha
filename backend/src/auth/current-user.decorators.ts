d4cd206c557c3549265a1beb9d582e382f0036fb
Ismael Mehdid <ismaelmehdidwork@gmail.com>
2025-10-30 19:48:28 +0100

    Remove unused decorator

--- backend/src/auth/current-user.decorators.ts
+++ backend/src/auth/current-user.decorators.ts
@@ -7,11 +7,3 @@ export const CurrentUser = createParamDecorator(
     return request.user;
   },
 );
-
-export const WsCurrentUser = createParamDecorator(
-  (data: string | undefined, ctx: ExecutionContext) => {
-    const client = ctx.switchToWs().getClient();
-    if (data) return client.data.user?.[data];
-    return client.data.user;
-  },
-);
